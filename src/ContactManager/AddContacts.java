ab5f5c6d47816043d2687bc2d8eea4c2300ea99b
Jeanette Perez <jeanette.marie.perez@gmail.com>
2021-03-22 15:23:16 -0500

    class for adding contacts

---
+++ src/ContactManager/AddContacts.java
@@ -0,0 +1,19 @@
+package ContactManager;
+
+public class AddContacts extends Contact {
+
+
+    public AddContacts(String firstName, String lastName, Integer phoneNumber) {
+        super(firstName, lastName, phoneNumber);
+    }
+
+
+
+
+
+
+
+
+
+
+}
