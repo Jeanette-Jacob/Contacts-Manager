e91e8f3cc7ba83bacd6a516421cd7eb0a05d400b
Jeanette Perez <jeanette.marie.perez@gmail.com>
2021-03-22 15:27:24 -0500

    adding getters and setters

--- src/ContactManager/Contact.java
+++ src/ContactManager/Contact.java
@@ -1,16 +1,41 @@
 package ContactManager;
 
 public class Contact {
-    private String lastName = "";
     private String firstName = "";
+    private String lastName = "";
     private int phoneNumber = 0;
-
 
     public Contact(String firstName, String lastName, Integer phoneNumber) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.phoneNumber = phoneNumber;
-
-
     }
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+
+
+
+
 }
